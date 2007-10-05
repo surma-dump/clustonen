5bb7a06b202fdd1794f21707a2a9c911d71a3aea
Andi Drebes <hackbert@drebesium.org>
2007-10-05 18:03:19 +0200

    Added class "Server"

---
+++ include/Server.h
@@ -0,0 +1,34 @@
+/**
+ * Copyright (C) 2007  Andi Drebes <hackbert@drebesium.org>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <string>
+#include "MessageManager.h"
+
+class Server {
+	public:
+		Server(const std::string& name);
+		MessageManager& getMessageManager();
+		
+	protected:
+		std::string name;
+		MessageManager mmgr;
+};
+
+#endif //SERVER_H
