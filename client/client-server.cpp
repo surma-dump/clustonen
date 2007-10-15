e34d9e8f27c57391c236d39a688bb0bc4cf59609
Andi Drebes <hackbert@drebesium.org>
2007-10-15 14:52:31 +0200

    [BUGFIX] The client-server now adds the other side to the list of clients

--- client/client-server.cpp
+++ client/client-server.cpp
@@ -203,6 +203,7 @@ int main(int argc, char* argv[])
 
 		//The server we're connected to is this local server's only client
 		Client client(sendSocket.getOpponent()+"-"+server_name);
+		srv.addClient(&client);
 
 		while (true)
 		{
